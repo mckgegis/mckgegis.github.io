5dc36960ef761854134f9e84ff2061a8142198a4
mckgegis <mckgegis@yahoo.com>
2020-04-19 21:25:24 -1000

    play button shift

--- sketch.js
+++ sketch.js
@@ -196,7 +196,7 @@
 
     const createButtons = () => {
       sketch.playButton = sketch.createButton("►");
-      sketch.playButton.position(640,2415);
+      sketch.playButton.position(644,2425);
       sketch.playButton.mousePressed(togglePlaying);
       sketch.playButton.addClass('control-button');
     };
